56286119a873e173a2b0860a22e9a18c6f77422b
Charmed <67402756+ever-nascent@users.noreply.github.com>
2023-05-01 12:45:49 -0700

    Update base_gui.py
    Added constant variables in BaseGUI

--- src/base_gui.py
+++ src/base_gui.py
@@ -1,15 +1,20 @@
 import tkinter as tk
 
+WINDOW_WIDTH = 275
+WINDOW_HEIGHT = 384
+BG_COLOR = "#484454"
+
 class BaseGUI:
+
     def __init__(self):
         self.root = tk.Tk()
         self.configure_window()
 
     def configure_window(self):
         self.root.title("Tally Counter")
-        self.root.geometry("275x384")
+        self.root.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
         self.root.resizable(False, False)
-        self.root.configure(background="#484454")
+        self.root.configure(background=BG_COLOR)
 
     def run(self):
         self.root.mainloop()
